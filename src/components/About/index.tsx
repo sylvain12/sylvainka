9780b79381fea4ee63d673117d362427ae98ec30
Sylvain Kadjo <sylvainka12@gmail.com>
2021-11-22 19:32:35 +0000

    Add global section title

---
+++ src/components/About/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "../Shared/Title";
+
+export default function About() {
+  return (
+    <div>
+      <Title barPosition="left">About me</Title>
+    </div>
+  );
+}
---
+++ src/components/Shared/Title.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode } from "react";
+import styled, { css } from "styled-components";
+
+interface TitleProps {
+  barPosition: string;
+  showBar: boolean;
+  showPonctuation: boolean;
+  children: ReactNode | ReactNode[];
+}
+
+const StyledTitle = styled.h3`
+  position: relative;
+  display: inline-block;
+  font-size: 3rem;
+  font-weight: var(--fw-light);
+
+  &::after {
+    content: '';
+    position: absolute;
+    background: var(--second-color);
+    height: 4px;
+    width: 5rem;
+    bottom: -1rem;
+    ${(props) =>
+  props.showBar && props.barPosition === "center" && css`
+      left: 50%;
+      transform: translateX(-50%);
+    `}
+
+    ${(props) =>
+  props.showBar && props.barPosition === "left" && css`
+      left: 0;
+    `}
+
+    ${(props) =>
+  props.showBar && props.barPosition === "right" && css`
+      right: 0;
+    `}
+  }
+`;
+
+const TitlePoctuation = styled.span`
+  /* display:  ${(props) => props.showPonctuation ? "inline-block" : "none"}; */
+  display: inline-block;
+  font-size: inherit;
+  color: var(--second-color);
+`;
+
+export default function Title(
+  { barPosition = "center", showBar = true, showPonctuation = true, children }:
+    TitleProps,
+) {
+  return (
+    <StyledTitle
+      barPosition={barPosition}
+      showBar={showBar}
+      showPoctuation={showPonctuation}
+    >
+      {children}
+      {showPonctuation ? <TitlePoctuation>.</TitlePoctuation> : ""}
+    </StyledTitle>
+  );
+}
