55c6a4a39395be115f6a64c9825bbe73c7f1538e
Sylvain Kadjo <sylvainka12@gmail.com>
2021-11-25 18:20:06 +0000

    Refactory Hero and its content

--- src/components/Hero/Hero.style.tsx
+++ src/components/Hero/Hero.style.tsx
@@ -1,82 +1,130 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import { HeroJobProps } from './Hero';
+import { HeroHilightProps } from './Hero';
 
 export const HeroWrapper = styled.div`
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+  text-align: center;
+  /* width: 40% */
+`;
+
+export const HeroIntroContainer = styled.div`
+  border-radius: .5em;
+  display: flex;
+  justify-content: space-between;
+  align-items:center;
+  text-align: left;
+  transform: translate(0px,-25px) rotate(-2deg);
 `;
 
 export const HeroIntro = styled.span`
-  font-size: 1.6rem;
-  margin-bottom: .3rem;
+  font-size: 2rem;
   font-weight: var(--fw-medium);
   color: var(--main-color);
   display: block;
-  padding-bottom: 1rem;
-  border-bottom: 1px solid var(--main-color-light);
+  display: inline-block;
+`;
+
+export const HeroTitleContainer = styled.div`
+  box-shadow: 0 0 1.2rem rgba(0,0,0,.2), 0 0 3rem rgba(96, 163, 217, 0.2);
+  background: var(--white-color);
+  border-radius: .5em;
+  position: relative;
+
+  &::before {
+    content:'';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 99%;
+    height: 95%;
+    border-radius: inherit;
+    background: var(--second-color);
+    z-index: -1;
+    transform: translate(5px,-14px) rotate(-2deg);
+  }
 `;
 
 export const HeroTitle = styled.h1`
   font-size: 2.5rem;
-  margin-bottom: .5rem;
+  margin-bottom: 2rem;
   color: var(--second-color);
 `;
 
 export const HeroSubIntro = styled.p`
-font-size: 3rem;
+font-size: 1.8rem;
 line-height: 1.7;
+width: 90%;
+max-width: 650px;
+margin-left: auto;
+margin-right: auto;
 `;
 
 export const HeroName = styled.span`
-  font-size: 2.2rem;
+  font-size: 3rem;
   display: inline-block;
   color: var(--main-color);
   font-weight: var(--fw-bold);
-  font-family: var(--ff);
+  font-family: var(--ff-variant);
   opacity: .65;
   text-transform: uppercase;
-  font-style: italic;
+  letter-spacing: 1rem;
+  display: flex;
+  align-items: center;
+  /* margin-left: 2rem; */
+  /* font-style: italic; */
 `;
 
 export const HeroJob = styled.p`
-  font-size: 6.5rem;
+  font-size: 5.5rem;
   display: inline-block;
-  flex: 0;
   font-weight: var(--fw-bold);
   position: relative;
   color: var(--main-color);
+  padding: .4rem;
 
-  &::before {
+  /* &::before {
     content: '';
     position: absolute;
     left: 0;
     bottom: 15px;
+    bottom: 0;
     background: var(--white-color);
     height: 1.5rem;
+    height: 100%;
     width: 100%;
-    z-index: -1;
-    border-radius: 4px;
-    box-shadow: 0 .6rem .10rem rgba(0,0,0, .2);
-  }
+    z-index: -1; */
+
+    /* ${({variant}: HeroJobProps) => variant === 'primary' && css`
+      background: var(--main-color); */
+      /* color: var(--white-color); */
+    /* `} */
+  /* }  */
 `;
 
 export const HeroHilight = styled.span`
   font-weight: var(--fw-bold);
   display: inline-block;
   position: relative;
-  font-size: 2.5rem;
+  font-size: 2.2rem;
 
   &::before {
     content: '';
     position: absolute;
     width: 100%;
-    height: 8px;
+    height: 7px;
     z-index: -1;
     left: 0;
     bottom: 10px;
     border-radius: 4px;
     background: var(--second-color);
+
+    ${({type}: HeroHilightProps) => type==='warning' && css`
+      background: #ffc107;
+    `}
   }
 `;
 
@@ -85,6 +133,8 @@ export const HeroDetail = styled.p`
   line-height: 1.7;
   display: inline-block;
   max-width: 580px;
+  width: 90%;
+  line-height: 1.7;
 `;
 
 export const HeroScroll = styled.div`
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import {
   HeroDetail,
   HeroHilight,
@@ -9,34 +9,69 @@ import {
   HeroSubIntro,
   HeroTitle,
   HeroWrapper,
+  HeroIntroContainer,
+  HeroTitleContainer,
 } from "./Hero.style";
 import { ArrowDown } from "iconoir-react";
 import Link from "next/link";
 
+
+export interface HeroJobProps {
+  variant?: string,
+  children: ReactNode
+}
+
+export interface HeroHilightProps{
+  type?: string,
+  children: ReactNode,
+}
+
+const StyleHeroJob = ({variant, children, ...rest}: HeroJobProps) => {
+  return (
+    <HeroJob variant={variant} {...rest} >{children}</HeroJob>
+  );
+}
+
+const StyledHeroHilight = ({type, children, ...rest}: HeroHilightProps) => {
+  return (
+    <HeroHilight type={type} {...rest}>{children}</HeroHilight>
+  )
+}
+
 export default function Hero() {
+
   return (
     <HeroWrapper>
-      <HeroIntro>
-        Hi, i am 
-        - <HeroName>Sylvain Kadjo</HeroName>
-      </HeroIntro>
 
-      <HeroTitle>
-        <HeroJob>Software developer</HeroJob>
-        <span style={{ fontSize: "7rem" }}>.</span>
-      </HeroTitle>
+      <HeroIntroContainer>
+        <HeroIntro>Hi, i am</HeroIntro>
+        <span style={{display: 'inline-block', height: "6px", width:"6rem", background: "#ff5f6f", borderRadius: "5px"}}></span>
+        <HeroName>
+          {/* <span style={{fontSize: '2rem'}}>⭐️</span> */}
+          Sylvain Kadjo
+          {/* <span style={{fontSize: '2rem'}}>⭐️</span> */}
+          </HeroName>
+        {/* <span></span> */}
+      </HeroIntroContainer>
+
+      <HeroTitleContainer>
+        <HeroTitle>
+          <StyleHeroJob>Software developer.</StyleHeroJob>
+        </HeroTitle>
+      </HeroTitleContainer>
 
       <HeroSubIntro>
+        I am a self-taught software developer with kwnolegde on <StyledHeroHilight type='warning'>design</StyledHeroHilight> & <StyledHeroHilight>data science</StyledHeroHilight>.
+        {/* I am a self-taught software developer, i work most of the time on <HeroHilight>web</HeroHilight> project. I sometime do some <HeroHilight>design</HeroHilight> and also work on <HeroHilight>data science</HeroHilight> project. */}
+{/*         
         & you know <HeroHilight>what</HeroHilight> ? I{" "}
         <span style={{ color: "red" }}>♥</span> build{" "}
-        <HeroHilight>Stuff</HeroHilight>
+        <HeroHilight>Stuff</HeroHilight> */}
       </HeroSubIntro>
 
-      <HeroDetail>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
-        consequatur assumenda repudiandae tenetur aspernatur nostrum similique
-        temporibus illum.
-      </HeroDetail>
+      {/* <HeroDetail>
+        
+      </HeroDetail> */}
 
       <HeroScroll>
         <Link href="#about">
